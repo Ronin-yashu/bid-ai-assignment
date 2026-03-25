46f0a6cd17e73f4a57a23af31f2f7d3faf6364fc
Ronin-yashu <yashu.jaat880@gmail.com>
2026-03-25 23:32:36 +0530

    chore: remove dead duplicate jobRoutes.js

--- backend/src/routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createJob, getJobs } from '../controllers/jobController.js';
-
-const router = express.Router();
-
-router.post('/job/create', createJob);
-router.get('/get/jobs', getJobs);
-
-export default router;
